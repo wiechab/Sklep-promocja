cdf14f89c0c2fcc9ffb96c9f163857cd46a61083
wiechab <wiechab@outlook.com>
2017-11-02 23:35:44 +0100

    2017-11-02

--- src/pl/edu/sklep/promo/CyztajPlik.java
+++ src/pl/edu/sklep/promo/CyztajPlik.java
@@ -6,7 +6,9 @@ package pl.edu.sklep.promo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -20,14 +22,19 @@ public class CyztajPlik {
 	/**
 	 * @param args
 	 */
+
+	private static Map<String, Integer> liczniki = new HashMap<>();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String plik = "C:\\dok.txt";
-	//	wczytajPlik(plik );
+		wczytajPlik(plik );
+		
+		//liczniki.forEach(o -> System.out.println(o));
 	}
-	protected Logger logger = Logger.getGlobal();
+	private static Logger logger = Logger.getGlobal();
 	
-	public void wczytajPlik(String source) {
+	private static void wczytajPlik(String source) {
 		try {
 			File file = new File(source);
 			logger.info("Wczytano: " + source + "\n");
@@ -39,13 +46,83 @@ public class CyztajPlik {
 			}
 			sc.close();
 			logger.info("Zakoñczono przetwarzanie: " + source + "\n");
+			
+			System.out.println(liczniki); //zwraca klucz, iloœæ wyst¹pieñ
+			System.out.println(liczniki.values()); //zwraca iloœæ wyst¹pieñ danego klucza
+			System.out.println(liczniki.keySet()); //zwraca klucze mapy
+			
+			String maxk = "";
+			Integer maxv = 0;
+			
+			String mink = "";
+			Integer minv = 1;
+			
+			for (Map.Entry<String, Integer> ll : liczniki.entrySet()) {
+				String k = ll.getKey();
+				Integer v = ll.getValue();
+				if(v >= maxv) {
+				
+					if(maxv == v) {
+						maxk = maxk + ", " + k;
+					} else {
+					maxk = k;
+				
+				}	
+					maxv = v;
+				
+				if(v <= minv) {
+					if(minv == v) {
+						mink = mink + ", " + k;
+					} else {
+						mink = k;
+					}
+						
+					minv = v;
+					
+				}
+			}
+		}
+			System.out.println("max " + maxk + " wyst¹pieñ " + maxv);
+			System.out.println("min " + mink + " wyst¹pieñ " + minv);
+			
 		} catch (FileNotFoundException e) {
 			logger.throwing("AWczytajPlik", "wczytajPlik", e);
 		}
 	}
-		public void przetwarzajLinie(String wiersz) {
+	private static void przetwarzajLinie(String wiersz) {
 			List<String> s³owa = Arrays.asList(wiersz.split(" "));
+			String[] names = new String[s³owa.size()];
+			/*
+		Collection<String> listUn = Collections.unmodifiableCollection(lista);
+		
+		listUn.forEach(o -> System.out.println(o));
+			 */
+			//for (int i = 0; i < s³owa.size(); i++) {
+
+				//}
+			int i = 0;
+			for ( String s : s³owa) {
+				//System.out.println("s=");
+				names[i] = s.replace("Java", "avaJ");
+				
+				//names[i] = s.replace(".", "");
+				//names[i] = s.replace(",", "");
+				System.out.println(names[i]);
+				liczniki.merge(names[i].toUpperCase(), 1, Integer::sum);
+				i++;
+
+				}
+
+			//System.out.println(liczniki.values()); //zwraca iloœæ wyst¹pieñ
+			//System.out.println(liczniki.get(s³owa));
+
+			/*
+			s³owa.forEach(o -> o.replace(".", ""));
+			s³owa.forEach(o -> o.replace(",", ""));
+			s³owa.forEach(o -> liczniki.merge(o, 1, Integer::sum));
 			
-			
+			s³owa.forEach(o -> System.out.println(o));
+			*/
+			//System.out.println(s³owa);
 		};
 }
